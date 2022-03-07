5c672defb7b4712a26c6e27f7c26c280e090ef64
ilandy.chiu <w9874123@gmail.com>
2022-03-07 14:21:40 +0800

    [功能] 針對增加品質的 function 抽為共用的 function
    (cherry picked from commit ea94f3d70886c839f1d0de0ef6d62d0dc5171604)

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -18,21 +18,15 @@ class Shop {
         return
       }
       if ( item.name === "Aged Brie") {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+        increaseQuality(item)
       } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
           if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+            increaseQuality(item)
           }
           if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+            increaseQuality(item)
           }
         }
       } else {
@@ -45,23 +39,29 @@ class Shop {
 
       if (item.sellIn < 0) {
         if (item.name === "Aged Brie") {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+          increaseQuality(item)
         } else {
           if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
             item.quality = item.quality - item.quality;
           } else {
             if (item.quality > 0) {
-              item.quality = item.quality - 1;
+            item.quality = item.quality - 1;
             }
           }
         }
       }
     }
+    
+    function increaseQuality (item) {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
 
     return this.items;
   }
+
+  
 }
 
 module.exports = {
