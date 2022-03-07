e00875719962bcb2c1e4ba11070ec6e0fe365f2b
ilandy.chiu <w9874123@gmail.com>
2022-03-07 14:58:35 +0800

    [功能] 抽出個品項的計算邏輯
    (cherry picked from commit c5923cad8d3f68433ca9043412e6ffdc517e7905)

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,46 +15,56 @@ class Shop {
 
     function updateItemQuality(item) {
       if (item.name === "Sulfuras, Hand of Ragnaros") {
-        return
+        return;
       }
-      item.sellIn = item.sellIn - 1;
 
-      if ( item.name === "Aged Brie") {
-        increaseQuality(item)
-        if(item.sellIn < 0) increaseQuality(item)
-
+      if (item.name === "Aged Brie") {
+        upgradeAgedBrie(item)
       } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-        
-          increaseQuality(item)
-          if (item.sellIn < 10) increaseQuality(item)
-          if (item.sellIn < 5) increaseQuality(item)
-          if (item.sellIn < 0) item.quality = item.quality - item.quality;
-
-        
+        upgradeBackstage(item)
       } else {
-        decreaseQuality(item)
-        if(item.sellIn < 0) {
-          decreaseQuality(item)
-        }
+        upgradeNormalItem(item)
       }
     }
-    
-    function increaseQuality (item) {
+
+    function increaseQuality(item) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
       }
     }
 
-    function decreaseQuality (item) {
+    function decreaseQuality(item) {
       if (item.quality > 0) {
         item.quality = item.quality - 1;
       }
     }
 
+    function upgradeAgedBrie (item) {
+      item.sellIn = item.sellIn - 1;
+
+      increaseQuality(item);
+      if (item.sellIn < 0) increaseQuality(item);
+    }
+
+    function upgradeBackstage (item) {
+      item.sellIn = item.sellIn - 1;
+
+      increaseQuality(item);
+      if (item.sellIn < 10) increaseQuality(item);
+      if (item.sellIn < 5) increaseQuality(item);
+      if (item.sellIn < 0) item.quality = item.quality - item.quality;
+    }
+
+    function upgradeNormalItem (item) {
+      item.sellIn = item.sellIn - 1;
+      decreaseQuality(item);
+      if (item.sellIn < 0) {
+        decreaseQuality(item);
+      }
+    }
+
     return this.items;
   }
-
-  
 }
 
 module.exports = {
