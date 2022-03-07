127866b450bdf344755bfce8249ffc8417a0190a
ilandy.chiu <w9874123@gmail.com>
2022-03-07 14:28:34 +0800

    [功能] 整合 sellin 的邏輯
    (cherry picked from commit cb92c1bda9369e3697ad72463c69b07226ba588b)

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -17,22 +17,24 @@ class Shop {
       if (item.name === "Sulfuras, Hand of Ragnaros") {
         return
       }
+      item.sellIn = item.sellIn - 1;
+
       if ( item.name === "Aged Brie") {
         increaseQuality(item)
+        if(item.sellIn < 0) increaseQuality(item)
+
       } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-          if (item.sellIn < 11) {
-            increaseQuality(item)
-          }
-          if (item.sellIn < 6) {
-            increaseQuality(item)
-          }
+
+          if (item.sellIn < 10) increaseQuality(item)
+          if (item.sellIn < 5) increaseQuality(item)
+          if (item.sellIn < 0) item.quality = item.quality - item.quality;
         }
+
+        
       } else {
         decreaseQuality(item)
-      }
-
       item.sellIn = item.sellIn - 1;
 
       if (item.sellIn < 0) {
@@ -44,6 +46,8 @@ class Shop {
           } else {
             decreaseQuality(item)
           }
+        if(item.sellIn < 0) {
+          decreaseQuality(item)
         }
       }
     }
